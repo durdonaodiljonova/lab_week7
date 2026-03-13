242283ea97412166012e881e233696f48f4a77c4
Durdona <odiljonovad3@gmail.com>
2026-03-13 18:13:35 +0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    String name;
+    Address address;
+    String email;
+    String phone;
+
+    public Person(String name, Address address, String email, String phone) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+}
