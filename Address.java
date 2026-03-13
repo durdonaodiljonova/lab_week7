d93ab620103c7964bcffb2f21d06a7e8bcb134d6
Durdona <odiljonovad3@gmail.com>
2026-03-13 18:13:14 +0500

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,16 @@
+public class Address {
+
+    String streetAddress;
+    String city;
+    String state;
+    String zipCode;
+    String country;
+
+    public Address(String streetAddress, String city, String state, String zipCode, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+}
