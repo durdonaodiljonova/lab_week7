4c5d9469c7bb3076deda35fdc268bc6b2e443ab1
Durdona <odiljonovad3@gmail.com>
2026-03-13 18:15:31 +0500

    Create Seat.java

---
+++ Seat.java
@@ -0,0 +1,12 @@
+public class Seat {
+
+    String seatNumber;
+    SeatType type;
+    SeatClass seatClass;
+
+    public Seat(String seatNumber, SeatType type, SeatClass seatClass) {
+        this.seatNumber = seatNumber;
+        this.type = type;
+        this.seatClass = seatClass;
+    }
+}
